48910bef87476f1b51bbcccfa5b372f3b8262c0b
JayGangkun <jaygangkun@hotmail.com>
2024-09-25 07:51:04 -0400

    no message

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -273,37 +273,46 @@ class Page extends BaseController
         }
 
         $query = $db->query("INSERT INTO applications(`application_user_id`, `location_apply_for`, `before_apply`, `age_18`, `how_hear`, `how_hear_referred`, `start_date`, `perform_essential_functions`, `restaurant_experience_food_preparation`, `restaurant_experience_sanitation`, `able_work_nights`, `food_handler_permit`, `obtain_food_handler_permit`, `available_to_work`, `companies`, `references`) VALUES('".($application_user_id)."', '".(isset($_POST["location_apply_for"]) ? $_POST["location_apply_for"] : '')."', '".(isset($_POST["before_apply"]) ? $_POST["before_apply"] : '')."', '".(isset($_POST["age_18"]) ? $_POST["age_18"] : '')."', '".(isset($_POST["how_hear"]) ? $_POST["how_hear"] : '')."', '".(isset($_POST["how_hear_referred"]) ? $_POST["how_hear_referred"] : '')."', '".(isset($_POST["start_date"]) ? $_POST["start_date"] : '')."', '".(isset($_POST["perform_essential_functions"]) ? $_POST["perform_essential_functions"] : '')."', '".(isset($_POST["restaurant_experience_food_preparation"]) ? $_POST["restaurant_experience_food_preparation"] : '')."', '".(isset($_POST["restaurant_experience_sanitation"]) ? $_POST["restaurant_experience_sanitation"] : '')."', '".(isset($_POST["able_work_nights"]) ? $_POST["able_work_nights"] : '')."', '".(isset($_POST["food_handler_permit"]) ? $_POST["food_handler_permit"] : '')."', '".(isset($_POST["obtain_food_handler_permit"]) ? $_POST["obtain_food_handler_permit"] : '')."', '".(isset($_POST["available_to_work"]) ? $_POST["available_to_work"] : '')."', '".json_encode($companies)."', '".json_encode($references)."')");
+        $application_id = $db->insertID();
         
         // send email
         if(isset($_POST['location_apply_for'])) {
             if($_POST['location_apply_for'] == '1') {
                 $to = 'clearlake@salata.com';
+                $store = 'Salata Clear Lake';
             }
             else if($_POST['location_apply_for'] == '2') {
                 $to = 'baybrook@salata.com';
+                $store = 'Salata Baybrook';
             }
             else if($_POST['location_apply_for'] == '3') {
                 $to = 'pasadena@salata.com';
+                $store = 'Salata Pasadena';
             }
             else if($_POST['location_apply_for'] == '4') {
                 $to = 'pinnaclepark@salata.com';
+                $store = 'Salata League City';
             }
             else if($_POST['location_apply_for'] == '5') {
                 $to = 'kingwood@salata.com';
+                $store = 'Salata Kingwood';
             }
             else if($_POST['location_apply_for'] == '6') {
                 $to = 'westlake@salata.com';
+                $store = 'Salata Westlake';
             }
             else if($_POST['location_apply_for'] == '7') {
                 $to = 'louettapines@salata.com';
+                $store = 'Salata Louetta Pines';
             }
             // $to = 'jaygangkun@hotmail.com';
+            // $to = 'munjeet.singh@gmail.com';
 
             $name = (isset($_POST["first_name"]) ? $_POST["first_name"] : '').' '.(isset($_POST["middle_name"]) ? $_POST["middle_name"] : '').' '.(isset($_POST["last_name"]) ? $_POST["last_name"] : '');
-            $link = base_url('/apply-view/'.$application_user_id);
+            $link = base_url('/apply-view/'.$application_id);
 
             if($to != '') {
-                sendMail($to, 'Submit New Application', '<h1>'.$name.'</h1><a href="'.$link.'">View</a>');
+                sendMail($to, 'Submit New Candidate', 'New Candidate named <strong>'.$name.'</strong> has submitted an application for '.$store.', click <a href="'.$link.'">here</a> to review');
             }
         }
         
